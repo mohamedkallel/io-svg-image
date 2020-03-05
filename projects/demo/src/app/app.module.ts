71517bcd0e27358b41d087e266becbd1df8076fc
mohamed <mohamed.kallel@iovision.tn>
2020-03-05 18:50:43 +0100

    fix issue

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SvgImageModule } from 'io-svg-image';
+import { SvgImageModule } from 'lib/src/public-api';
 
 @NgModule({
   declarations: [
